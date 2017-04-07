82e4e3f2cef96351acc81834c30f33d3f8300af9
Omar Alshammary <omar.alshammary@hotmail.com>
2017-04-07 15:37:08 +0300

    Routing Chapter
    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,46 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { RouterModule } from '@angular/router'
 
-import { AppComponent }  from './app.component';
+import { AppComponent }        from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component'
+import { HeroService } from './hero.service'
+import { DashboardComponent } from './dahsboard.component'
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  bootstrap: [ AppComponent ],
+  providers: [HeroService]
 })
+
 export class AppModule { }
